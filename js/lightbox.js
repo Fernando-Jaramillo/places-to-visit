dc9f6c2cc7e99db80fdf19331969bb958774c98f
Fernando <lenin_jaramillo@hotmail.com>
2020-07-29 17:00:17 -0500

    closing container function

--- js/lightbox.js
+++ js/lightbox.js
@@ -2,18 +2,22 @@ const imagenes = document.querySelectorAll('.img-galeria');
 const imagenLight = document.querySelector('.agregar-imagen');
 const contenedorLight = document.querySelector(".imagen-light");
 
-console.log(imagenes);
-console.log(imagenLight);
-console.log(contenedorLight);
-
 imagenes.forEach(imagen => {
     imagen.addEventListener("click", () => {
         aparecerImagen(imagen.getAttribute('src'));
     })
 });
 
+contenedorLight.addEventListener("click", e => {
+    if(e.target != imagenLight){
+        contenedorLight.classList.toggle('show');
+        imagenLight.classList.toggle('showImage');
+    }
+})
+
 const aparecerImagen = (imagen) => {
     imagenLight.src = imagen;
-    contenedorLight.classList.toggle('show')
-    imagenLight.classList.toggle('showImage')
+    contenedorLight.classList.toggle('show');
+    imagenLight.classList.toggle('showImage');
 }
+
