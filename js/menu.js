46a219a4df29a93b3a43592b2805c33a9afa841c
Fernando <lenin_jaramillo@hotmail.com>
2020-07-29 20:05:24 -0500

    burger efect

--- js/menu.js
+++ js/menu.js
@@ -6,10 +6,12 @@ const menu = document.querySelector('.menu-navigation');
 
 hamburguer.addEventListener('click', () => {
     menu.classList.toggle("spread");
+    hamburguer.style.opacity = "0";
 });
 
 window.addEventListener('click', e => {
     if (menu.classList.contains('spread') && e.target != hamburguer && e.target != menu){
         menu.classList.toggle("spread");
+        hamburguer.style.opacity = '1';
     }
 })
