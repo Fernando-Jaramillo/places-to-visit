d24e0a5ec19d577fae8fd508398dd43c9d787558
Fernando <lenin_jaramillo@hotmail.com>
2020-08-15 13:05:46 -0500

    changed format

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
-<!-- This comment line needed for bootstrap to work on mobile devices -->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Places to go</title>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
